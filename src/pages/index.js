e368c5a51404ed5716cea8869c85d4afc4f88242
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2018-09-04 14:30:11 -0700

    Installed sass preprocessor plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import './Index.scss'
+  
+export default () => (
+  <div className="container">
+    <h1>Hello world!</h1>
+    <p>CSS preprocessors are pretty great.</p>
+  </div>
+);
