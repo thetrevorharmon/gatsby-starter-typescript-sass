d76364e50e17a6d5a2eb4823b757bcdc2c514ef6
pkino <ikuhide131610@yahoo.co.jp>
2020-05-13 17:14:20 +0900

    Fix gatsby-config and package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     name: `Hello Typescript World!`,
-    tagline: `Gatsby + SASS + Typescript`
+    tagline: `Gatsby + SASS + Typescript`,
   },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
-    `gatsby-plugin-eslint`
+    `gatsby-plugin-eslint`,
   ],
-}
+};
